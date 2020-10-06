03260c2d19dba1948c9a774873812a56387abf79
Patrick <PatrickChenuwo@gmail.com>
2020-10-05 21:42:11 -0600

    Finished functions on product form

--- Travel_Experts_CS/frmProduct.Designer.cs
+++ Travel_Experts_CS/frmProduct.Designer.cs
@@ -28,91 +28,172 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
       this.productDataGridView = new System.Windows.Forms.DataGridView();
-      this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-      this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
       this.btnNewProduct = new System.Windows.Forms.Button();
       this.label1 = new System.Windows.Forms.Label();
-      ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+      this.btnEdit = new System.Windows.Forms.Button();
+      this.SupplierInGridView = new System.Windows.Forms.DataGridView();
+      this.SupplierOutGridView = new System.Windows.Forms.DataGridView();
+      this.btnClose = new System.Windows.Forms.Button();
+      this.lblSupplierIn = new System.Windows.Forms.Label();
+      this.lblSupplierOut = new System.Windows.Forms.Label();
+      this.btnDelete = new System.Windows.Forms.Button();
+      this.btnAdd = new System.Windows.Forms.Button();
       ((System.ComponentModel.ISupportInitialize)(this.productDataGridView)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.SupplierInGridView)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.SupplierOutGridView)).BeginInit();
       this.SuspendLayout();
-      // 
-      // productBindingSource
-      // 
-      this.productBindingSource.DataSource = typeof(Travel_Experts_CS.Product);
       // 
       // productDataGridView
       // 
-      this.productDataGridView.AutoGenerateColumns = false;
+      this.productDataGridView.AllowUserToAddRows = false;
+      this.productDataGridView.AllowUserToDeleteRows = false;
       this.productDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.productDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn2});
-      this.productDataGridView.DataSource = this.productBindingSource;
-      this.productDataGridView.Location = new System.Drawing.Point(26, 50);
+      this.productDataGridView.Location = new System.Drawing.Point(26, 32);
       this.productDataGridView.Name = "productDataGridView";
-      this.productDataGridView.Size = new System.Drawing.Size(295, 220);
+      this.productDataGridView.ReadOnly = true;
+      this.productDataGridView.RowHeadersVisible = false;
+      this.productDataGridView.Size = new System.Drawing.Size(295, 200);
       this.productDataGridView.TabIndex = 1;
-      // 
-      // dataGridViewTextBoxColumn1
-      // 
-      this.dataGridViewTextBoxColumn1.DataPropertyName = "ProductId";
-      this.dataGridViewTextBoxColumn1.HeaderText = "ProductId";
-      this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-      // 
-      // dataGridViewTextBoxColumn2
-      // 
-      this.dataGridViewTextBoxColumn2.DataPropertyName = "ProdName";
-      this.dataGridViewTextBoxColumn2.HeaderText = "ProdName";
-      this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+      this.productDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.productDataGridView_CellClick);
       // 
       // btnNewProduct
       // 
-      this.btnNewProduct.Location = new System.Drawing.Point(381, 50);
+      this.btnNewProduct.Location = new System.Drawing.Point(372, 69);
       this.btnNewProduct.Name = "btnNewProduct";
       this.btnNewProduct.Size = new System.Drawing.Size(121, 23);
       this.btnNewProduct.TabIndex = 2;
-      this.btnNewProduct.Text = "New Product";
+      this.btnNewProduct.Text = "&New  Product";
       this.btnNewProduct.UseVisualStyleBackColor = true;
       // 
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(26, 31);
+      this.label1.Location = new System.Drawing.Point(26, 13);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(77, 16);
       this.label1.TabIndex = 3;
       this.label1.Text = "Product List";
       // 
+      // btnEdit
+      // 
+      this.btnEdit.Location = new System.Drawing.Point(372, 129);
+      this.btnEdit.Name = "btnEdit";
+      this.btnEdit.Size = new System.Drawing.Size(121, 23);
+      this.btnEdit.TabIndex = 4;
+      this.btnEdit.Text = "&Edit  Product";
+      this.btnEdit.UseVisualStyleBackColor = true;
+      // 
+      // SupplierInGridView
+      // 
+      this.SupplierInGridView.AllowUserToAddRows = false;
+      this.SupplierInGridView.AllowUserToDeleteRows = false;
+      this.SupplierInGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.SupplierInGridView.Location = new System.Drawing.Point(26, 286);
+      this.SupplierInGridView.Name = "SupplierInGridView";
+      this.SupplierInGridView.ReadOnly = true;
+      this.SupplierInGridView.RowHeadersVisible = false;
+      this.SupplierInGridView.Size = new System.Drawing.Size(256, 201);
+      this.SupplierInGridView.TabIndex = 5;
+      this.SupplierInGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SupplierInGridView_CellClick);
+      // 
+      // SupplierOutGridView
+      // 
+      this.SupplierOutGridView.AllowUserToAddRows = false;
+      this.SupplierOutGridView.AllowUserToDeleteRows = false;
+      this.SupplierOutGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.SupplierOutGridView.Location = new System.Drawing.Point(372, 286);
+      this.SupplierOutGridView.Name = "SupplierOutGridView";
+      this.SupplierOutGridView.ReadOnly = true;
+      this.SupplierOutGridView.RowHeadersVisible = false;
+      this.SupplierOutGridView.Size = new System.Drawing.Size(256, 201);
+      this.SupplierOutGridView.TabIndex = 6;
+      this.SupplierOutGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SupplierOutGridView_CellClick);
+      // 
+      // btnClose
+      // 
+      this.btnClose.Location = new System.Drawing.Point(553, 129);
+      this.btnClose.Name = "btnClose";
+      this.btnClose.Size = new System.Drawing.Size(75, 23);
+      this.btnClose.TabIndex = 7;
+      this.btnClose.Text = "&Close";
+      this.btnClose.UseVisualStyleBackColor = true;
+      this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+      // 
+      // lblSupplierIn
+      // 
+      this.lblSupplierIn.Location = new System.Drawing.Point(26, 249);
+      this.lblSupplierIn.Name = "lblSupplierIn";
+      this.lblSupplierIn.Size = new System.Drawing.Size(256, 36);
+      this.lblSupplierIn.TabIndex = 8;
+      // 
+      // lblSupplierOut
+      // 
+      this.lblSupplierOut.Location = new System.Drawing.Point(369, 249);
+      this.lblSupplierOut.Name = "lblSupplierOut";
+      this.lblSupplierOut.Size = new System.Drawing.Size(259, 36);
+      this.lblSupplierOut.TabIndex = 9;
+      // 
+      // btnDelete
+      // 
+      this.btnDelete.Location = new System.Drawing.Point(288, 346);
+      this.btnDelete.Name = "btnDelete";
+      this.btnDelete.Size = new System.Drawing.Size(75, 23);
+      this.btnDelete.TabIndex = 10;
+      this.btnDelete.Text = ">>>";
+      this.btnDelete.UseVisualStyleBackColor = true;
+      this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+      // 
+      // btnAdd
+      // 
+      this.btnAdd.Location = new System.Drawing.Point(288, 406);
+      this.btnAdd.Name = "btnAdd";
+      this.btnAdd.Size = new System.Drawing.Size(75, 23);
+      this.btnAdd.TabIndex = 11;
+      this.btnAdd.Text = "<<<";
+      this.btnAdd.UseVisualStyleBackColor = true;
+      this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+      // 
       // frmProduct
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(849, 554);
+      this.ClientSize = new System.Drawing.Size(661, 505);
+      this.Controls.Add(this.btnAdd);
+      this.Controls.Add(this.btnDelete);
+      this.Controls.Add(this.lblSupplierOut);
+      this.Controls.Add(this.lblSupplierIn);
+      this.Controls.Add(this.btnClose);
+      this.Controls.Add(this.SupplierOutGridView);
+      this.Controls.Add(this.SupplierInGridView);
+      this.Controls.Add(this.btnEdit);
       this.Controls.Add(this.label1);
       this.Controls.Add(this.btnNewProduct);
       this.Controls.Add(this.productDataGridView);
       this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.Margin = new System.Windows.Forms.Padding(4);
       this.Name = "frmProduct";
-      this.Text = "frmProduct";
+      this.Text = "Products";
       this.Load += new System.EventHandler(this.frmProduct_Load);
-      ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.productDataGridView)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.SupplierInGridView)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.SupplierOutGridView)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
     }
 
     #endregion
-
-    private System.Windows.Forms.BindingSource productBindingSource;
     private System.Windows.Forms.DataGridView productDataGridView;
-    private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-    private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
     private System.Windows.Forms.Button btnNewProduct;
     private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button btnEdit;
+    private System.Windows.Forms.DataGridView SupplierInGridView;
+    private System.Windows.Forms.DataGridView SupplierOutGridView;
+    private System.Windows.Forms.Button btnClose;
+    private System.Windows.Forms.Label lblSupplierIn;
+    private System.Windows.Forms.Label lblSupplierOut;
+    private System.Windows.Forms.Button btnDelete;
+    private System.Windows.Forms.Button btnAdd;
   }
 }
--- Travel_Experts_CS/frmProduct.cs
+++ Travel_Experts_CS/frmProduct.cs
@@ -10,8 +10,18 @@ using System.Windows.Forms;
 
 namespace Travel_Experts_CS
 {
+  /// <summary>
+  /// Purpose: Source code for the form Product
+  /// Author: Pengfei (Patrick) Chen
+  /// Date: October, 2020
+  /// </summary>
   public partial class frmProduct : Form
   {
+    //  database context used in this form
+    private TravelExpertDataDataContext dbContext = new TravelExpertDataDataContext();
+    private int nProductID = 0;   //  Product ID
+    private int nProdSuppID = 0;  //  Product_Supplier ID
+    private int nSupplierID = 0;  //  Supplier ID
     public frmProduct()
     {
       InitializeComponent();
@@ -19,10 +29,226 @@ namespace Travel_Experts_CS
 
     private void frmProduct_Load(object sender, EventArgs e)
     {
-      TravelExpertDataDataContext dbContext = new TravelExpertDataDataContext();
-      productDataGridView.DataSource = (from prod in dbContext.Products
-                                        orderby prod.ProdName
-                                        select prod).ToList();
+      //  Set the data source for the product data grid view
+      try
+      {
+        productDataGridView.DataSource = (from prod in dbContext.Products
+                                          orderby prod.ProdName
+                                          select new
+                                          {
+                                            ID = prod.ProductId,
+                                            Product = prod.ProdName
+                                          }).ToList();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message, ex.GetType().ToString());
+      }
+
+      //  set the column width for the product data grid view
+      productDataGridView.Columns[0].Width = 50;
+      productDataGridView.Columns[1].Width = 200;
+
+      //  set the first row as the default product
+      nProductID = Convert.ToInt32(productDataGridView["ID", 0].Value);
+      string strProductName = productDataGridView["Product", 0].Value.ToString();
+
+      //  display the grid title for the two supplier lists
+      lblSupplierIn.Text = "Suppliers provide the\n" + strProductName + " services:";
+      lblSupplierOut.Text = "Suppliers do NOT provide the\n" + strProductName + " services:";
+
+      //  display the data for the two supplier lists
+      DisplaySupplierOnProduct(nProductID);
     }
-  }
-}
+
+    private void productDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+      //  set the product ID based on the selected row from the grid view
+      int rowNum = Convert.ToInt32(productDataGridView.CurrentCell.RowIndex);
+      nProductID = Convert.ToInt32(productDataGridView["ID", rowNum].Value);
+
+      //  display the grid title for the two supplier lists
+      string strProductName = productDataGridView["Product", rowNum].Value.ToString();
+      lblSupplierIn.Text = "Suppliers provide the\n" + strProductName + " services:";
+      lblSupplierOut.Text = "Suppliers do NOT provide the\n" + strProductName + " services:";
+
+      //  display the data for the two supplier lists
+      DisplaySupplierOnProduct(nProductID);
+    }
+
+    /// <summary>
+    /// Purpose: display and refresh the two supplier lists
+    /// author: Pengfei (Patrick) Chen
+    /// Date: October 2020
+    /// </summary>
+    /// <param name="nProductID">The current product ID for the form</param>
+    private void DisplaySupplierOnProduct(int nProductID)
+    {
+      //  set the data source for the two supplier lists
+      try
+      {
+        SupplierInGridView.DataSource = (from pro_sup in dbContext.Products_Suppliers
+                                join supp in dbContext.Suppliers
+                                  on pro_sup.SupplierId equals supp.SupplierId
+                                where pro_sup.ProductId == nProductID
+                                orderby supp.SupName
+                                select new
+                                {
+                                  ID = pro_sup.ProductSupplierId,
+                                  Name = supp.SupName
+                                }).ToList();
+
+        SupplierOutGridView.DataSource = (from Supplier in dbContext.Suppliers
+                                          where !(from prod_sup in dbContext.Products_Suppliers
+                                                  where prod_sup.ProductId == nProductID
+                                                  select prod_sup.SupplierId).Contains(Supplier.SupplierId)
+                                          orderby Supplier.SupName
+                                          select new
+                                          {
+                                            ID = Supplier.SupplierId,
+                                            Name = Supplier.SupName
+                                          }).ToList();
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show(exception.Message, exception.GetType().ToString());
+      }
+
+      //  set the width for the 2 data grid views
+      SupplierInGridView.Columns[0].Width = 50;
+      SupplierInGridView.Columns[1].Width = 180;
+      SupplierOutGridView.Columns[0].Width = 50;
+      SupplierOutGridView.Columns[1].Width = 180;
+
+      //  reset the 2 IDs to force users to select the item from the list to set IDs,
+      //  which can avoid accidentally delete / add data in the database
+      nProdSuppID = 0;
+      nSupplierID = 0;
+    }
+
+    //  Update the Product_Supplier ID based on the selection of the grid view
+    private void SupplierInGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+      int rowNum = Convert.ToInt32(SupplierInGridView.CurrentCell.RowIndex);
+      nProdSuppID = Convert.ToInt32(SupplierInGridView["ID", rowNum].Value);
+    }
+
+    //  Update the Supplier ID based on the selection of the grid view
+    private void SupplierOutGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+      int rowNum = Convert.ToInt32(SupplierOutGridView.CurrentCell.RowIndex);
+      nSupplierID = Convert.ToInt32(SupplierOutGridView["ID", rowNum].Value);
+    }
+
+    //  Delete a Product_Supplier relation in the database
+    private void btnDelete_Click(object sender, EventArgs e)
+    {
+      //  no supplier selected, give the user a warning message and return
+      if(nProdSuppID == 0)
+      {
+        MessageBox.Show("Please select a Supplier from the List on the left-hand side please!");
+        return;
+      }
+
+      //  get the current product Name and supplier Name for warning messages below
+      int nRowNum = Convert.ToInt32(productDataGridView.CurrentCell.RowIndex);
+      string strProductName = productDataGridView["Product", nRowNum].Value.ToString();
+      nRowNum = Convert.ToInt32(SupplierInGridView.CurrentCell.RowIndex);
+      string strSupplierName = SupplierInGridView["Name", nRowNum].Value.ToString();
+
+      //  Check if the product-supplier ID was attached to some packages.
+      var objPackages = (from pkg in dbContext.Packages
+                         join pps in dbContext.Packages_Products_Suppliers
+                         on pkg.PackageId equals pps.PackageId
+                         where pps.ProductSupplierId == nProdSuppID
+                         select pkg.PkgName).ToList();
+
+      //  if the product-supplier ID was attached to some packages, give warning message and return
+      if (objPackages.Count > 0)
+      {
+        string strPackageList = String.Empty;
+        foreach(string pkg in objPackages)
+          strPackageList += pkg + "\n";
+        MessageBox.Show("The following packages have " +
+          strProductName + " service provided by "+ strSupplierName +":\n" +
+          strPackageList + "Please change the packages first.");
+        return;
+      }
+
+      //  confirmation with user, return if user choose "No"
+      if (MessageBox.Show("Are you sure the company " +
+        strSupplierName + " does not provide the " + strProductName + " service any more?",
+        "Confirmation", MessageBoxButtons.YesNo) == DialogResult.No)
+        return;
+
+      //  Delete the record from database
+      try
+      {
+        var objToBeDeleted = (from prod_supp in dbContext.Products_Suppliers
+                              where prod_supp.ProductSupplierId == nProdSuppID
+                              select prod_supp).FirstOrDefault();
+        if (objToBeDeleted != null)
+        {
+          dbContext.Products_Suppliers.DeleteOnSubmit(objToBeDeleted);
+          dbContext.SubmitChanges();
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("Some error happened: " + ex.Message, ex.GetType().ToString());
+      }
+
+      //  refresh the 2 supplier lists
+      DisplaySupplierOnProduct(nProductID);
+
+    }
+
+    //  add a new Product_supplier relation to the database
+    private void btnAdd_Click(object sender, EventArgs e)
+    {
+      //  if the current product ID is zero, give warning message and return
+      if (nProductID == 0)
+      {
+        MessageBox.Show("Please select a product from the list first!");
+        return;
+      }
+
+      //  if the current supplier ID is zero, give warning message and return
+      if (nSupplierID == 0)
+      {
+        MessageBox.Show("Please select a supplier from the right-hand side list first!");
+        return;
+      }
+
+      //  check if the record in the database already
+      var objCheckDup = (from ps in dbContext.Products_Suppliers
+                         where ps.ProductId == nProductID && ps.SupplierId == nSupplierID
+                         select ps).FirstOrDefault();
+      //  if the record in the database already, refresh the 2 supplier list and return
+      if (objCheckDup != null)
+      {
+        MessageBox.Show("Someone has added this supplier already, please check it again!");
+        DisplaySupplierOnProduct(nProductID);
+        return;
+      }
+
+      //  insert the new record to the database
+      Products_Supplier objProdSupp = new Products_Supplier
+      {
+        ProductId = nProductID,
+        SupplierId = nSupplierID
+      };
+      dbContext.Products_Suppliers.InsertOnSubmit(objProdSupp);
+      dbContext.SubmitChanges();
+
+      //  refresh the 2 supplier list
+      DisplaySupplierOnProduct(nProductID);
+    }
+
+    //  close the Product form
+    private void btnClose_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+  } //  end of class
+} //  end of namespace
